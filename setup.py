e2c94c0d97e61d0401afe4b1bc1c899aa531e362
hmcparlane <hayden.mcparlane@gmail.com>
2016-06-15 23:01:00 -0500

    Added server auto-install script to install dependencies

---
+++ setup.py
@@ -0,0 +1,18 @@
+#
+#	Automated setup script for Roku Streak
+#	@author: Hayden McParlane
+#	@creation-date: 6.15.2016
+
+# Install eclipse if necessary
+
+# Install oracle jdk 8u91 if necessary
+
+# Set environment
+
+# JAVA_HOME
+
+# Install necessary eclipse plugin
+
+# Setup eclipse environment
+
+# 
